c29e8971d2ff23032c563563e2376a91fca89450
kylieanyce <kylieanyce@gmail.com>
2021-05-14 10:50:20 -0500

    added navbar and router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom"
 import './index.css';
-import GamerRater from './components/GamerRater'
+import { GamerRater } from './components/GamerRater.js'
 
 ReactDOM.render(
   <React.StrictMode>
-    <GamerRater />
+    <Router>
+      <GamerRater />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
