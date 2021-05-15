d920b4d23528c03988e5f15a482dc002c0c42b0e
kylieanyce <kylieanyce@gmail.com>
2021-05-15 08:50:41 -0500

    working on adding categories to games

---
+++ src/components/game/CategoryProvider.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react"
+
+export const CategoryContext = React.createContext()
+
+export const CategoryProvider = (props) => {
+    const [categories, setCategories] = useState([])
+
+    const getCategories = () => {
+        return fetch("http://localhost:8000/games", {
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("gr_token")}`
+            }
+        })
+            .then(response => response.json())
+            .then(setCategories)
+    }
+
+    return (
+        <CategoryContext.Provider value={{
+            categories, getCategories
+        }}>
+            {props.children}
+        </CategoryContext.Provider>
+    )
+}
--- src/components/game/GameDetails.js
+++ src/components/game/GameDetails.js
@@ -1,31 +1,31 @@
 import React, { useState, useContext, useEffect } from "react"
-import { useHistory, useParams } from "react-router"
+import { useParams } from "react-router"
 import { GameContext } from "./GameProvider.js"
+import { CategoryContext } from "./CategoryProvider.js"
 
 export const GameDetail = () => {
     const { getGameById } = useContext(GameContext)
+    const { categories, getCategories } = useContext(CategoryContext)
     const [game, setGame] = useState({})
     const { gameId } = useParams()
-    const history = useHistory()
 
     useEffect(() => {
         getGameById(gameId)
             .then((res) => {
                 setGame(res)
-            })
+            }).then(getCategories)
     }, [])
 
     return (
         <section className="gameDetails">
             <h3>{game.title}</h3>
+            <div className="game__description">{game.description}</div>
             <div className="game__players">Players needed: {game.num_of_players}</div>
+            <div className="game__age">Age Recommendation: {game.age_recommendation}</div>
             <div className="game__playtime">Play Time: {game.game_time_length}</div>
             <div className="game__designer">Designer: {game.designer}</div>
             <div className="game__year">Year Released: {game.year_released}</div>
-            <div className="game__age">Age Recommendation: {game.age_recommendation}</div>
-
+            <div className="game__categories">Categories: {game.categoryId}</div>
         </section>
     )
-
-
 }
